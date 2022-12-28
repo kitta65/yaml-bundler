4d4ebba527dc62b8bf2c9b275b57fc85b79410b5
dr666m1 <skndr666m1@gmail.com>
2022-12-29 00:25:31 +0900

    parse command  line argument

--- yamlbundler/command.py
+++ yamlbundler/command.py
@@ -1,4 +1,22 @@
+import argparse
+
+
 def main() -> str:
     s = "hello"
     print(s)
     return s
+
+
+def args() -> None:
+    parser = argparse.ArgumentParser(
+        prog="YAML Bundler", description="Handle !include tag and bundle YAML files."
+    )
+    parser.add_argument("filename")
+    parser.add_argument("-i", "--inplace", action="store_true")
+    parser.add_argument("-o", "--out")
+    args = parser.parse_args()
+    print(str(args))
+
+
+if __name__ == "__main__":
+    args()
